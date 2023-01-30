f15bdae0f4c3581bea8d4ed0205cfaf8dc113392
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2023-01-30 20:08:00 -0300

    Update 001.3
    - Add main.js

--- main.js
+++ main.js
@@ -3,20 +3,54 @@ const keys = document.querySelectorAll('.key')
 
 // play notes
 function playNote(event) {
-    console.log(event)
-    // keyCode 
+
+    let audioKeyCode = getKeyCode(event);
 
     // typed  or pressed key
+    const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
 
     //if key exists
+    const cantFoundAnyKey = !key
 
-    // play audio
+    if(cantFoundAnyKey) {
+        return;
+    }
 
+    addPlayingClass(key)
+    playAudio (audioKeyCode)
+    
+}
+
+function addPlayingClass(key) {
+    key.classList.add('playing')
+}
+
+function getKeyCode(event) {
+    let keyCode;
+
+    const isKeyboard = event.type === "keydown"
+    if(isKeyboard) {
+        keyCode = event.keyCode
+    } else {
+        keyCode = event.target.dataset.key
+    }
+    return keyCode
+}
+
+function playAudio (audioKeyCode) {
+    const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`)
+    audio.currentTime = 0;
+    audio.play()
+}
+
+function removePlayingClass(event) {
+    event.target.classList.remove("playing")
 }
 
 // click with mouse
 keys.forEach( function(key) {
     key.addEventListener("click", playNote)
+    key.addEventListener("transitionend", removePlayingClass)
 })
 
 // keyboard type
